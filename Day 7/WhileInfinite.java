b21d05e206221a8a0a7f6156148c4d9a4e2768cc
Shahzada <md.shahzada1606@gmail.com>
2021-12-05 15:26:17 +0530

    while loop

---
+++ Day 7/WhileInfinite.java
@@ -0,0 +1,18 @@
+/*
+Syntax for infinite while loop.
+
+while(true)
+{
+    // Statements to be executed.
+}
+*/
+
+public class WhileInfinite 
+{
+    public static void main(String[] args) {
+        while(true)
+        {
+            System.out.println("Infinite");
+        }
+    }    
+}
---
+++ Day 7/WhileLoop.java
@@ -0,0 +1,20 @@
+
+// Write a program to define the concept of while loop.
+
+/*
+When we need to iterate a part of the program and the no. of iteration is not fixed then it is 
+recommended to use the while loop.
+*/
+
+public class WhileLoop 
+{
+    public static void main(String[] args) {
+        int i = 1;
+
+        while(i<=10)
+        {
+            System.out.println(i);
+            i++;
+        }
+    }    
+}
