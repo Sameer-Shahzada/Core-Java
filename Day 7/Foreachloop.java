81130064bf7431686928c21ed067a32b3db71825
Shahzada <md.shahzada1606@gmail.com>
2021-12-05 15:14:59 +0530

    Practise loop program

---
+++ Day 7/Foreachloop.java
@@ -0,0 +1,16 @@
+// Write a program to define the for each loop 
+
+// For each loop is used to traverse an array or collection in java.
+
+
+public class Foreachloop 
+{
+    public static void main(String[] args) {
+        int[] arr = {12, 34, 45, 44,32, 78};
+
+        for(int i: arr)
+        {
+            System.out.println(i);
+        }
+    }
+}
---
+++ Day 7/Infinitivefor.java
@@ -0,0 +1,17 @@
+
+// Infinitive for loop
+/*
+If we put two ;; in the for loop syntax then it execute the loop in infinite times such as -
+for(;;)
+*/
+
+public class Infinitivefor {
+    public static void main(String[] args) {
+        for(;;)
+        {
+            System.out.println("Infinitive loop");
+
+            // Use CTRL + C to stop the infinte loop.
+        }
+    }
+}
