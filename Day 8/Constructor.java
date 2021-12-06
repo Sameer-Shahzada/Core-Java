d206be093461bac08e07687655964a0c22d3307a
Shahzada <md.shahzada1606@gmail.com>
2021-12-06 23:10:01 +0530

    Default Constructor

---
+++ Day 8/Constructor.java
@@ -0,0 +1,14 @@
+// Write a program to understand the concept of Default Constructor.
+
+class Student 
+{
+    Student()
+    {
+        System.out.println("Default Constructor");
+    }
+    public static void main(String[] args) {
+        // Create the instance of the class
+        Student s1 = new Student();
+        
+    }
+}
