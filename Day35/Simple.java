a987bb048e6cf0fd23687c4c5893c5debceda60b
Shahzada <md.shahzada1606@gmail.com>
2022-01-25 22:38:12 +0530

    Package

---
+++ Day35/Simple.java
@@ -0,0 +1,22 @@
+
+
+/* 
+    Java Packages 
+        -> package is a group of similar type of classes, interface and sub-packages.
+        -> Package is used to categorized into two forms -
+            1- Built-in packages(Pre-defined packages)
+                Ex - util , sql ,lang etc.
+            2- User-defined packages
+                -> User-defined packages are those packages which is create or defined by user.
+        
+    //-> Write a simple program of packages 
+*/
+
+package Day35;     // packages keyword is used to create packages in java.
+
+public class Simple 
+{
+    public static void main(String[] args) {
+        System.out.println("Welcome to the package");
+    }
+}
