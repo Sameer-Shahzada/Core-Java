2c84292bb25f1be625a4ca9bc40d3dae642c0e98
Shahzada <md.shahzada1606@gmail.com>
2022-02-01 22:07:32 +0530

    Recursion

---
+++ Day42/Recursion.java
@@ -0,0 +1,12 @@
+package Day42;
+public class Recursion {  
+
+    static void p(){  
+    System.out.println("hello");  
+    p();  
+    }  
+      
+    public static void main(String[] args) {  
+    p();  
+    }  
+}
